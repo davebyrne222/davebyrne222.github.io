4be24f7f7a31600e3bebd02665c72bbee187aa48
David Byrne <davidbyrne@Davids-MBP.station>
2023-07-13 21:07:58 +0100

    first commit

---
+++ src/components/body/about/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./about.css";
+import SocialContact from "../../common/social-contact/index";
+function About() {
+  return (
+    <div className="about">
+      <div className="about-top">
+        <div className="about-info">
+        Hi,
+         <br /> Im <span className="info-name">Dave</span>.
+         <br /> Physicist turned Scrum Master turned Back-End Developer
+        </div>
+        <div className="about-photo">
+          <img 
+            src={require("../../../assets/coding.png")}
+            className="picture"
+          />
+        </div>
+      </div>
+      <SocialContact />
+    </div>
+  );
+}
+
+export default About;
