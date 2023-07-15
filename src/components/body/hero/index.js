59c27b825e1a3bb278fa7e2e1c5ff6880967e36f
David Byrne <david.byrne222@gmail.com>
2023-07-15 17:56:54 +0100

    updated hero and footer

--- src/components/body/hero/index.js
+++ src/components/body/hero/index.js
@@ -6,23 +6,31 @@ function Hero() {
     <div className="hero">
       <div className="hero-top">
         <div className="hero-info">
-          Hi, I&#39;m <span className="info-name">Dave</span>.
-          <br /> Physicist, Scrum Master, <strong>Back-End Developer</strong>
+          Hi, I&#39;m <span className="info-name">Dave</span>
+          <div className="hero-tagline">
+            <label className="hero-prof-old">Physicist</label>{" "}
+            <label className="hero-prof-old">Scrum Master</label> [ Back-End ]{" "}
+            <strong> Developer</strong>
+          </div>
           <div className="hero-info-subheader">
-            "Huh!? Thats a weird mix of experience" - I agree!
-            <br /> See <a href="#work">About</a> for a better explanation
+            <div className="hero-quote">
+              "Huh!? Thats a weird mix of experience" <br /> - You
+            </div>
+            <br /> I agree! See <a href="#about">About</a> for a better
+            explanation
           </div>
           <div className="hero-info-body">
             I'm an IT professional with over 10 years experience in the software
-            development industry, mainly as a Scrum Master. I'm now making the
-            move to full-time developer
+            development industry, primarily as a Scrum Master. I'm now
+            transitioning to full-time developer
+          </div>
+          <div className="hero-info-body">
+            Please read my portfolio, check-out my experience, and contact me
+            with any opportunities you may have
           </div>
         </div>
         <div className="hero-photo">
-          <img
-            src={require("../../../assets/coding.png")}
-            className="picture"
-          />
+          <img src={require("../../../assets/hero.png")} className="picture" />
         </div>
       </div>
       <SocialContact />
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./footer.css";
 function Footer() {
-  return <div className="footer">Originally made by <a href="https://github.com/ThantZinPhyo-Slark/React-Portfolio">ThantZinPhyo</a>, Modified by David Byrne</div>;
+  return (
+    <div className="footer">
+      Originally made by
+      <a
+        style={{ marginLeft: 2 }}
+        href="https://github.com/ThantZinPhyo-Slark/React-Portfolio"
+      >
+        ThantZinPhyo
+      </a>
+      , Modified by
+      <a
+        style={{ marginLeft: 2 }}
+        href="https://github.com/davebyrne222/davebyrne222.github.io"
+      >
+        DaveByrne222
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
