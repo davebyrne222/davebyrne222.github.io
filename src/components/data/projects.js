35b66a477a04e97e75c47a8c2cc617fcff3715c5
David Byrne <david.byrne222@gmail.com>
2023-07-17 19:50:13 +0100

    updated project descriptions

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,10 +1,10 @@
 export const ProjectData = [
   {
     id: 1,
-    title: "This Page!",
-    tagline:
-      "This page was origionally created by ThantZinPhyo (see footer for a link) but I modified it to suit my preferences. Nothing to write home about but hey, its something!",
-    about: "",
+    title: "Github Pages (This Page)",
+    tagline: "A React Single-Page App with Material UI",
+    about:
+      "Origionally created by ThantZinPhyo (see footer for a link), I modified the page contents to showcase my experience and skills, adding Material UI (mui) for some added nice UI components.",
     tags: ["React"],
     demo: "",
     github: "https://github.com/davebyrne222/davebyrne222.github.io",
@@ -15,7 +15,7 @@ export const ProjectData = [
     title: "PI Planning Tools",
     tagline: "A Command Line Tool for Reviewing SAFe Program Increments",
     about:
-      "An example of using python to help my everyday job as a scrum master, this script generated a series of overviews (in the terminal) of how iterations in a PI were loaded, the progress made towards each feature, identified any potential issues with the stories, and helped to identify how the load related to the teams capacity, ensuring we always planned to enable the best delivery from a team.",
+      "An example of using python to help my everyday job as a scrum master, this script generates a series of overviews that A. help to ensure a successful PI planning and, B. to help track progress during a PI e.g. iteration story point load vs. capacity, feature development progress, and identified any potential issues with stories.",
     tags: ["Python", "Jira", "API"],
     demo: "",
     github: "https://github.com/davebyrne222",
@@ -26,7 +26,7 @@ export const ProjectData = [
     title: "Songs I'm Listening To",
     tagline: "A FastAPI and React App I built for fun and learning",
     about:
-      "I wanted to learn more about API development in Python, API security, React, ORM, analytics, and unit testing. So, I involved another love of mine - music. I built this web-app that displays some of the songs I have been listening to recently that have resonated. I hope that long term, this will help me with finding those elusive songs I spend an age trying to remember the name of.  ",
+      "To learn more about API development in Python, API security, React, ORM, analytics, and unit testing, I involved another love of mine - music. I built this web-app that displays some of the songs I have been listening to recently. I hope that long term, this will help me with finding those elusive songs I spend an age trying to remember the name of.",
     tags: ["Python", "FastAPI", "React", "OAuth", "Redis", "DataDog"],
     demo: "",
     github: "https://github.com/davebyrne222",
