37e9f53a5b9076407ec8ee19df0a586d59367f4e
David Byrne <david.byrne222@gmail.com>
2023-07-18 20:49:22 +0100

    updated about

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -19,9 +19,12 @@ function About() {
         Three reasons:
         <ul>
           <li>
-            I have always enjoyed 'coding' and was somewhat jealous of the
-            developers I worked with, often wanting to understand more of what
-            they were working on and at times, even contrinuting
+            I have always enjoyed 'coding'; As an undergrad, I studied C and C++
+            and caught the 'bug'. I then moved on to python and continued to use
+            it wherever I could, be it work or personal projects. As a scrum
+            master, I was always interested in what the developers wwre doing,
+            often wanting to understand more of what they were working on and at
+            times, even contributing
           </li>
           <li>
             I miss working on a technical challenges - as a physicist, I enjoyed
@@ -29,7 +32,7 @@ function About() {
             python)
           </li>
           <li>
-            The time is right - I have just been made redundant (layedoff) and
+            The time is right - I have just been made redundant (layed-off) and
             coincidentally had recently started studying computer science in
             Dublins 'Dublin Business School' as a hobby so, I felt it was the
             right time to make the change
@@ -45,37 +48,35 @@ function About() {
         research and work, so-much-so that my first coding experience was as an
         undergraduate student first studying Matlab, and then C & C++ for
         numerical analaysis. As a masters student, I then used python for data
-        analysis and modelling (my thesius involved using python-wrapped fortran
-        code)
+        analysis and modelling
       </p>
       <p>
         As a scrum master, I spent 10 years working very closely with a number
         of development teams across a broad range of projects including
-        industrial automation & IoT, speech technocology for education, and most
+        industrial automation & IoT, speech technology for education, and most
         recently in the travel industry connecting travel agents, airlines,
-        hotels, etc. During this time, I became intemately involved with what it
-        takes to make great software, from requirements generation to design,
-        from prototyping to full CI/CD pipelined implementations, from
-        dark-releases to full global customer releases (and the inevitable
-        outages)
+        hotels, etc. During this time, I became intemately involved in the
+        development lifecycle, from requirements generation to design, from
+        prototyping to full CI/CD pipelined implementations, from dark-releases
+        to global customer releases (and the inevitable outages)
       </p>
       <p>
         And during that time, I even contributed to some software projects
         directly and even built some proprietary applications myself.
       </p>
 
-      <h2 className="section-header">"So your a seasoned developer"</h2>
+      {/* <h2 className="section-header">"So your a seasoned developer"</h2>
       <p>
         Not at all. I can read code and understand complex implementations but
         to date, I have only developed 'simple' applications. Additionally, as I
         am largely self-taught, I suspect I have some learning to do when it
         comes to best-practices. That being said, I am not starting from scratch
         even though it may seem that way on paper.
-      </p>
+      </p> */}
 
       <h2 className="section-header">How did I get here?</h2>
       <p>
-        After finishing college, I went to work in the pharmaceutical industry
+        After finishing college, I began working in the pharmaceutical industry
         as a physicist. The company I worked for was small and so as somebody
         who had some coding experience, I began to work with the developers,
         mainly providing some technical expertise for hardware control software
@@ -88,16 +89,17 @@ function About() {
         While there, I again used python to conduct data analysis as my role as
         a techncial expert but I also later personally developed two
         customer-facing applications. The first was an automation system
-        developer in python that could be used to control pharmacutical
+        developed in python that could be used to control pharmacutical
         processes via python scripts and websockets. The second was a desktop
         application written in PyQT and R that could analyse chemometric data
-        and build predictive models using partial-least squares modelling.
+        and build predictive models using partial-least squares regression
+        modelling.
       </p>
       <p>
-        Again, being in a small company, everybody wears many hats and the time
-        came for me to focus, and I decided to focus on being a scrum master - A
-        role I loved for a long time, but I can't help but miss the technical
-        side of things. And this brings us to today.
+        Again, being in a small company, everybody wears many hats and I decided
+        to focus on being a scrum master, moving on to other roles in other
+        companies. I loved being a scrum master for a long time, but I can't
+        help but miss the technical side of things.
       </p>
     </div>
   );
